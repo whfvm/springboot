445ad76caaf1c3cebd8214eec921d7dc0a412bdc
yunhwan <yunhwan.choo00@gmail.com>
2024-05-19 15:33:36 +0900

    initial commit

---
+++ src/main/java/wthfmv/bandwith/BandwithApplication.java
@@ -0,0 +1,13 @@
+package wthfmv.bandwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandwithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandwithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wthfmv/bandwith/BandwithApplicationTests.java
@@ -0,0 +1,13 @@
+package wthfmv.bandwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
