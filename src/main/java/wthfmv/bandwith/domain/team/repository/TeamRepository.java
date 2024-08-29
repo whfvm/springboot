669a5446e1a24e6e41fbf0c7c98cb393728c2f44
yunhwan <yunhwan.choo00@gmail.com>
2024-08-29 13:43:51 +0900

    feat: 로그인, 팀 기능 1차 구현

--- src/main/java/wthfmv/bandwith/domain/team/repository/TeamRepository.java
+++ src/main/java/wthfmv/bandwith/domain/team/repository/TeamRepository.java
@@ -3,7 +3,6 @@ package wthfmv.bandwith.domain.team.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wthfmv.bandwith.domain.team.entity.Team;
-
 import java.util.UUID;
 
 @Repository
---
+++ src/main/java/wthfmv/bandwith/global/security/cors/CorsConfig.java
@@ -0,0 +1,34 @@
+package wthfmv.bandwith.global.security.cors;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry
+                .addMapping("/**")
+                .allowedOriginPatterns(allowedOrigins)
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .exposedHeaders("accessToken", "refreshToken");
+    }
+
+//    @Bean
+//    public CorsFilter corsFilter(){
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        for(String origin : allowedOrigins){
+//
+//        }
+//    }
+}
