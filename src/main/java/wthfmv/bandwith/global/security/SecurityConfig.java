1671b6912e8c5a01eeb3ad6a4ce85401abdd71bf
yunhwan <yunhwan.choo00@gmail.com>
2024-11-08 20:15:01 +0900

    swagger에서 토큰 사용

--- src/main/java/wthfmv/bandwith/global/security/SecurityConfig.java
+++ src/main/java/wthfmv/bandwith/global/security/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import wthfmv.bandwith.global.security.jwt.JwtAuthenticationFilter;
+import wthfmv.bandwith.global.security.filter.JwtAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
