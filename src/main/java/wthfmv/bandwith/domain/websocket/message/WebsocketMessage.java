439a1f0ba666c54545d1fe02f62a3b1cc6f37748
yunhwan <yunhwan.choo00@gmail.com>
2024-11-11 00:44:56 +0900

    Track 응답 시 update key 추가, 실시간 update 1차

--- src/main/java/wthfmv/bandwith/domain/websocket/message/WebsocketMessage.java
+++ src/main/java/wthfmv/bandwith/domain/websocket/message/WebsocketMessage.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class WebsocketMessage {
     private String trackId; //mongo 에 저장된 트랙의 id
-    private String query; // mongo 에 적용할 쿼리
-    private WebSocketMessageMethod webSocketMessageMethod;
+    private String[] query; // mongo 에 적용할 쿼리
+    private Object[] value;
+    private WebSocketMessageMethod[] webSocketMessageMethod;
+    private String uuid;
 }
